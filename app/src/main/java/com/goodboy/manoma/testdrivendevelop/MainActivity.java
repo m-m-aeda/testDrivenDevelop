dd3dfcba2856423ea9d0ed662f7efe556f3415cb
前田光貴 <mitsukim@maedakoukinoiMac.local>
2018-03-25 16:56:54 +0900

    色々。

---
+++ app/src/main/java/com/goodboy/manoma/testdrivendevelop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.goodboy.manoma.testdrivendevelop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
